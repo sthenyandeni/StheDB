204c02883afc027ec98bdd8e470186c427678b94
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-11-23 10:25:50 +0200

    Added table test code

--- src/main/main.c
+++ src/main/main.c
@@ -1,8 +1,17 @@
 #include "database.h"
+#include "table.h"
 
 int main(int argc, char const *argv[])
 {
     struct Database* db =  database_init();
     printf("Database name: %s\n", db->name);
+
+    struct Table* table = init_table("users.stf", "users");
+    printf("Table name: %s\n", table->name);
+    for (int i = 0; i < table->attr_count; i++) {
+        printf("Attr name %d: %s\n", i, table->attributes[i]->name);
+        printf("Attr size %d: %d\n", i, table->attributes[i]->size);
+        printf("Attr type %d: %d\n", i, table->attributes[i]->type);
+    }
     return 0;
 }
