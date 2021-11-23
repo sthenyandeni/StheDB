a7a13e88467d826a9b67e7b3350e53da7fa6ed87
Sithembiso Nyandeni <u19091479@tuks.co.za>
2021-11-23 10:25:42 +0200

    Created init_table

---
+++ src/main/table.c
@@ -0,0 +1,35 @@
+#include "table.h"
+
+struct Table* init_table(char* filename, char* name) {
+    FILE* table_format = fopen(filename, "r");
+    struct Table* table = malloc(sizeof(struct Table));
+    table->name = name;
+
+    fseek(table_format, 0, SEEK_SET);
+    int8_t attr_count = fgetc(table_format);
+    table->attr_count = attr_count;
+    table->attributes = malloc(sizeof(struct Attribute*) * attr_count);
+
+    //Ideally, this line would not be necessary
+    //TODO: Explore storing everything in hash tables
+    fseek(table_format, 1, SEEK_SET);
+    for (int i = 0; i < attr_count; i++) {
+        struct Attribute* attribute = malloc(sizeof(struct Attribute));
+
+        int8_t attribute_name_length = fgetc(table_format);
+        attribute->name = malloc((sizeof(char) * attribute_name_length) + 1);
+        fread(attribute->name, sizeof(char), attribute_name_length, table_format);
+        attribute->name[attribute_name_length] = '\0';
+        
+        int16_t attribute_type = fgetc(table_format);
+        attribute->type = attribute_type;
+
+        int16_t attribute_size = fgetc(table_format);
+        attribute->size = attribute_size;
+        
+
+        table->attributes[i] = attribute;
+    }
+
+    return table;
+}
---
+++ src/main/table.h
@@ -0,0 +1,10 @@
+#ifndef TABLE_H
+#define TABLE_H
+
+#include "types.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Table* init_table(char* filename, char* name);
+
+#endif
